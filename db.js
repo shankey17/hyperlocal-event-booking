aaec0b34ef0f3ba3e104e902ed3e12be37d0052a
shankey17 <83934959+shankey17@users.noreply.github.com>
2026-04-05 21:38:54 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT),
-
+  uri: process.env.DATABASE_URL,
   ssl: {
-    ca: process.env.DB_SSL_CA.replace(/\\n/g, '\n')
-  },
-
-  connectTimeout: 30000
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
